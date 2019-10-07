d3c616e739896d16416571191e9d9743bd9aa7ab
Kara Odle <karajo.odle@gmail.coml>
2019-10-07 13:22:03 -0700

    got pageAction to work

---
+++ background.js
@@ -0,0 +1,27 @@
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'learn.co' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+// Called when the user clicks on the browser action.
+chrome.pageAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});
--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,1 @@
-const intervalId = setInterval(() => {
-  const widget = document.getElementById("js--friends-widget")
-  if (widget) {
-    widget.style.display = "none"
-    clearInterval(intervalId)
-  }
-}, 500);
 
-
-
-// document.getElementById("js--friends-widget").style.display = "none"
