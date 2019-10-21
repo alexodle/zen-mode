23b5f03f6cabe4f1ee67499d630c882875d956b8
Kara Odle <karajo.odle@gmail.coml>
2019-10-21 09:27:42 -0700

    working with chrome storage api

--- background.js
+++ background.js
@@ -1,8 +1,28 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({checked: true}, function() {
-      console.log("Zen Mode is turned on!");
+  console.log("INSTALLED!!!!")
+    chrome.storage.sync.set({socialChecked: false}, function() {
+      console.log("Zen Mode social features are all turned off!");
+    });
+    chrome.storage.sync.set({lessonChecked: false}, function() {
+      console.log("Zen Mode lesson survey feature is turned off!");
+    });
+    chrome.storage.sync.set({questionChecked: false}, function() {
+      console.log("Zen Mode ask a question feature is turned off!");
     });
   });
+
+  chrome.runtime.onStartup.addListener(function() {
+    console.log("STARTUP!!!!")
+      chrome.storage.sync.set({socialChecked: false}, function() {
+        console.log("Zen Mode social features are all turned off!");
+      });
+      chrome.storage.sync.set({lessonChecked: false}, function() {
+        console.log("Zen Mode lesson survey feature is turned off!");
+      });
+      chrome.storage.sync.set({questionChecked: false}, function() {
+        console.log("Zen Mode ask a question feature is turned off!");
+      });
+    });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
