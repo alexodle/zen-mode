2675115f9ccfa834da97266388b1480c1139c249
Kara Odle <karajo.odle@gmail.coml>
2019-10-20 22:27:53 -0700

    commit before switch back to v2

--- contentScriptHide.js
+++ contentScriptHide.js
@@ -1,5 +1,7 @@
-document.getElementById("js--friends-widget").style.display = "none"
 document.getElementsByClassName("bubble-bar__label")[0].style.display = "none"
 document.getElementsByClassName("bubble-bar__list")[0].style.display = "none"
+
+document.getElementById("js--friends-widget").style.display = "none"
+
 document.getElementById("js--region-sidebar-additional-content").style.display = "none"
 document.getElementsByClassName("site-sidebar__actions")[0].style.display = "none"
